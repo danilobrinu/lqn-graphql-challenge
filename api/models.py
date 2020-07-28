aefaa2e0a2bd86271b3fac242f653c0892039ee3
danilobrinu <ddbn.c2@gmail.com>
2020-07-28 05:25:07 -0500

    Add api app

--- api/models.py
+++ api/models.py
@@ -1,3 +1,12 @@
+# Built-in packages
+from uuid import uuid4
+
+# Third-party package
 from django.db import models
 
-# Create your models here.
+# Local Packages
+from .domain.starship.models import *
+from .domain.person.models import *
+from .domain.planet.models import *
+from .domain.episode.models import *
+from .domain.human.models import *
---
+++ api/schema.py
@@ -0,0 +1,36 @@
+# Built-in packages
+
+# Third-party packages
+from graphene import ObjectType
+
+# Local packages
+from .domain.starship.schema import StarshipQuery, StarshipMutation
+from .domain.person.schema import PersonQuery, PersonMutation
+from .domain.planet.schema import PlanetQuery, PlanetMutation
+from .domain.episode.schema import EpisodeQuery, EpisodeMutation
+from .domain.human.schema import HumanQuery, HumanMutation
+from .domain.droid.schema import DroidQuery, DroidMutation
+
+
+class Query(
+    StarshipQuery,
+    PlanetQuery,
+    PersonQuery,
+    EpisodeQuery,
+    HumanQuery,
+    DroidQuery,
+    ObjectType,
+):
+    pass
+
+
+class Mutation(
+    StarshipMutation,
+    PlanetMutation,
+    EpisodeMutation,
+    HumanMutation,
+    DroidMutation,
+    ObjectType,
+):
+    pass
+
