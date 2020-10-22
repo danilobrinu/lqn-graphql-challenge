05a36d08653de620599e9b1e00b8b1c4f2606f22
danilobrinu <ddbn.c2@gmail.com>
2020-10-22 18:23:34 -0500

    Update utils

--- api/utils.py
+++ api/utils.py
@@ -1,4 +1,5 @@
 # Built-in packages
+from typing import Annotated, Any
 
 # Third-party packages
 from django.db.models import Model
@@ -9,16 +10,43 @@ from graphene_django.filter import DjangoFilterConnectionField
 
 
 class BaseModel(Model):
-    def update(self, **kwargs):
+    @staticmethod
+    def create(
+        self,
+    ) -> Annotated[
+        Any, "create a new object with the given data and returns that object"
+    ]:
+        self.save()
+
+        return self
+
+    @staticmethod
+    def update(
+        self, **kwargs
+    ) -> Annotated[
+        Any, "update a given object with the given data and returns that object"
+    ]:
         for field, value in kwargs.items():
             setattr(self, field, value)
-        return self.save()
+        self.save()
+
+        return self
+
+    @staticmethod
+    def destroy(
+        self,
+    ) -> Annotated[Any, "delete a given object and returns that object"]:
+        self.delete()
+
+        return self
 
     class Meta:
         abstract = True
 
 
-def create_open_crud_filter_connection(name, filterset_class):
+def create_open_crud_filter_connection(
+    name, filterset_class
+) -> Annotated[Any, "Custom DjangoFilterConnectionField for OpenCrud connections"]:
     @classmethod
     def resolve_queryset(
         cls, connection, iterable, info, args, filtering_args, **kwargs
