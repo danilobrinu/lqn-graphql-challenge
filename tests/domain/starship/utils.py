1afe257e629ea175f2920b5663e4bc38afb02610
danilobrinu <ddbn.c2@gmail.com>
2020-10-22 21:10:57 -0500

    Update tests domain starship

--- tests/domain/starship/utils.py
+++ tests/domain/starship/utils.py
@@ -6,9 +6,10 @@ from faker import Faker
 # Local packages
 from api.domain.starship.data import create_starship
 
+faker = Faker()
+
 
 def create_random_starship():
-    faker = Faker()
     starship = create_starship(
         {"name": faker.name(), "length": faker.pyfloat(2, 2, True)}
     )
