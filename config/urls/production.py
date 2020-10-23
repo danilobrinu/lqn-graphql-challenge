f3e4303938dcf980e45d6144d6d8f52368a7a37e
danilobrinu <ddbn.c2@gmail.com>
2020-10-23 15:29:20 -0500

    Minor changes

--- config/urls/production.py
+++ config/urls/production.py
@@ -5,7 +5,7 @@ from django.views.decorators.csrf import csrf_exempt
 from graphene_django.views import GraphQLView
 
 # Local packages
-from .common import *  # noqa
+from .common import *  # noqa skipcq: PYL-W0614
 
 urlpatterns += [
     path("graphql/", csrf_exempt(GraphQLView.as_view())),
