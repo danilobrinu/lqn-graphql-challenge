7aed37355424dab76929953e5c38c1d4ad67dce0
danilobrinu <ddbn.c2@gmail.com>
2020-10-22 21:09:15 -0500

    Update utils models

--- api/utils/models.py
+++ api/utils/models.py
@@ -1,5 +1,5 @@
 # Built-in packages
-from typing import Annotated, Any
+from typing import Any
 
 # Third-party packages
 from django.db.models import Model
@@ -8,31 +8,20 @@ from django.db.models import Model
 
 
 class BaseModel(Model):
-    def create(
-        self,
-    ) -> Annotated[
-        Any, "create a new object with the given data and returns that object"
-    ]:
+    def create(self,) -> Any:
         self.id = None
-        self.pk = None
         self.save()
 
         return self
 
-    def update(
-        self, **kwargs
-    ) -> Annotated[
-        Any, "update a given object with the given data and returns that object"
-    ]:
+    def update(self, **kwargs) -> Any:
         for field, value in kwargs.items():
             setattr(self, field, value)
         self.save()
 
         return self
 
-    def destroy(
-        self,
-    ) -> Annotated[Any, "delete a given object and returns that object"]:
+    def destroy(self,) -> Any:
         self.delete()
 
         return self
